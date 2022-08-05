6d7f985c9d94093609339f260046a32d669ea15f
Eyetu Kingsley <eyetukingsley330@gmail.com>
2022-08-05 23:29:47 +0100

    I have adjusted the products grid tag and also added the category links and pages.

--- products/templatetags/products_display.py
+++ products/templatetags/products_display.py
@@ -1,4 +1,5 @@
 from django import template
+from products.models import Product
 
 register = template.Library()
 
@@ -12,4 +13,13 @@ def product_slider(product_list):
 
 @register.inclusion_tag("products/product_grid.html")
 def products_grid(product_list):
+    try:
+        product_list = list(product_list)
+        for item in product_list:
+            if type(item) != Product:
+                item_index = product_list.index(item)
+                product_list[item_index] = item.product
+    except:
+        product_list = product_list
+        
     return {"product_list": product_list}
